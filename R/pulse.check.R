aaff64c5f391941183117ee1d8be3edae16647cb
phamdn <48892803+phamdn@users.noreply.github.com>
2025-11-19 14:52:04 +0100

    0.0.33

--- R/pulse.check.R
+++ R/pulse.check.R
@@ -8,7 +8,16 @@
 #' @export
 #'
 #' @examples
-pulse.check <- function(data, channel, dttm.limits = c(NA, NA)){
+pulse.check <- function(data, channel,
+                        dttm.limits = c(NA, NA),
+                        sampling.rate = NULL, cor.threshold = 0.4
+                        ){
+
+  # infer sampling rate Hz based on input data
+  if (is.null(sampling.rate)) {
+    sampling.rate <- round(1/median(as.numeric(diff(data$datetime))))
+    message("using sampling rate of ", sampling.rate, " Hz")
+  }
 
   fig1 <- ggplot(data, aes(x = datetime, y = .data[[channel]])) +
     geom_line() +
@@ -17,6 +26,14 @@ pulse.check <- function(data, channel, dttm.limits = c(NA, NA)){
 
   print(fig1)
 
-  subset(data, datetime >= dttm.limits[1] & datetime <= dttm.limits[2], channel)
+  signal <- subset(data, datetime >= dttm.limits[1] & datetime <= dttm.limits[2], channel)
+
+  output <- pulse.hr(signal = signal,
+                     sampling.rate = sampling.rate,
+                     cor.threshold = cor.threshold)
+
+  plot(output$ac.list)
+
+  output
 
 }
