f8d31264d252267177910dedb0b9eaab941e569f
phamdn <48892803+phamdn@users.noreply.github.com>
2026-08-11 22:48:10 +0200

    0.3.6

--- R/add.datetime.R
+++ R/add.datetime.R
@@ -4,16 +4,28 @@
 #'
 #' @param df a data frame.
 #' @param timezone a character string, local time zone.
-#' @param reverse a logical, whether to add datetime.UTC column.
+#' @param reverse a logical, whether to convert local datetime to UTC datetime.
 #'
 #' @returns
 #' @export
 #'
 #' @examples
+#' # example a
+#' df <- data.frame(datetime.UTC = as.POSIXct(c("2025-01-01 01:00:00", "2025-07-01 01:00:00"), tz = "UTC"))
+#' df2 <- add.datetime(df, timezone = "")
+#' df2
+#' df2$datetime.UTC
+#' df2$datetime
+#'
+#' # example b
+#' df3 <- data.frame(datetime = as.POSIXct(c("2025-02-01 01:00:00", "2025-08-01 01:00:00")))
+#' df3$datetime
+#' df4 <- add.datetime(df3)
+#' df4
 add.datetime <- function(df, timezone, reverse = FALSE) {
 
   if (!reverse){
-    if (!"datetime" %in% names(df)) { # if datetime missing
+    if ("datetime" %notin% names(df)) { # if datetime missing
       df <- df %>%
         mutate(
           datetime = as.POSIXct(.data$datetime.UTC, tz = timezone),
@@ -23,7 +35,7 @@ add.datetime <- function(df, timezone, reverse = FALSE) {
       message("converting UTC to ", ifelse(timezone == "", Sys.timezone(), timezone), " time")
     }
 
-    if (!"date" %in% names(df) || !"time" %in% names(df)) { # if date or time missing
+    if ("date" %notin% names(df) || "time" %notin% names(df)) { # if date or time missing
       df <- df %>%
         mutate(
           date = as_date(.data$datetime),
