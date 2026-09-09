c68f23bbdd09be9616278436db5012a5cae85ecf
phamdn <48892803+phamdn@users.noreply.github.com>
2026-09-09 17:39:27 +0200

    0.5.1
    robo.read() revised

--- R/robo.read.R
+++ R/robo.read.R
@@ -5,26 +5,29 @@
 #' @param file.path a character string, path to the file.
 #' @param metadata.lines an integer, number of lines to skip in the header.
 #' @param timezone a character string, time zone.
-#' @param summary.period a character string, duration to summarize the mean of the records. Optional.
+#' @param agg.res a character string, duration to summarize the mean of the records. Optional.
 #'
-#' @returns a list of two data frames, \code{enhanced.data} and \code{summarized.data} for enhanced and summarized records, respectively.
+#' @returns a list of two data frames, \code{enhanced.data} and \code{aggregated.data} for enhanced and summarized records, respectively.
 #' @export
 #'
 #' @examples
 #' robo.file <- system.file("extdata/robo/RM1.csv", package = "rambur")
-#' robo.read(robo.file)
-#' robo.read(robo.file, summary.period = "hour")
+#' robo.read(robo.file, agg.res = "1 hour")
 robo.read <- function(file.path,
                       metadata.lines = 21,
                       timezone = "",
-                      summary.period = NULL){
+                      agg.res = NULL){
 
   # notice about time zone
-  if (timezone == "") {
-    message("using ", Sys.timezone(), " time zone")
-  }
+  message("reminder: robo log files were in UTC")
 
-  # metadata
+  # notice about time zone
+  if (timezone == "")
+    message("assuming ", Sys.timezone(), " as local time zone")
+  else
+    message("using ", timezone, " as local time zone")
+
+  # retain metadata
   metadata <- paste(read_lines(file = file.path, n_max = metadata.lines),
                     collapse = "\n")
 
@@ -33,14 +36,10 @@ robo.read <- function(file.path,
   # read_csv uses UTC as default (see col_datetime() and locale()),
   # which is the correct tz of robomussel (always UTC+0000)
 
-  enhanced.data <- original.data %>%
+  enhanced.data <- original.data |>
     transmute(datetime.UTC = .data$time,
-              # datetime = format(time, tz = timezone), not working, just <chr> format
-              datetime = as.POSIXct(.data$datetime.UTC, tz = timezone),
-              date = as_date(.data$datetime),
-              time = as_hms(.data$datetime), # as.Date is base R but as_date and as_hms is not
-              temp = .data$temp
-           ) # transmute() is better than mutate() for keeping columns in desired order, note the repurposed use of "time"
+              temp = .data$temp) |>
+    add.datetime(type = 3, timezone = timezone)
 
   output <- list(
     metadata = metadata,
@@ -48,18 +47,14 @@ robo.read <- function(file.path,
     enhanced.data = enhanced.data
   )
 
-  if (!is.null(summary.period)) {
-    summarized.data <- enhanced.data %>%
-      mutate(datetime = floor_date(.data$datetime, summary.period)) %>%
-      group_by(.data$datetime) %>%
-      summarize(n = sum(!is.na(.data$temp)),
-                temp = mean(.data$temp)) %>%
-      mutate(date = as_date(.data$datetime),
-             time = as_hms(.data$datetime),
-             .after = .data$datetime
-      )
+  if (!is.null(agg.res)) {
+    aggregated.data <- enhanced.data %>%
+      mutate(datetime.UTC = floor_date(.data$datetime.UTC, agg.res)) %>%
+      group_by(.data$datetime.UTC) %>%
+      summarize(temp = mean(.data$temp)) %>%
+      add.datetime(type = 3, timezone = timezone)
 
-    output$summarized.data <- summarized.data
+    output$aggregated.data <- aggregated.data
 
   }
 
